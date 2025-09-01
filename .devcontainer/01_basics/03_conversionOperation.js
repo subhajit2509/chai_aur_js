f2c9025739978a6a3933f95d6f4ec05e52a8764e
subhajit2509 <subhajitchess25@gmail.com>
2025-09-01 13:35:13 +0000

    convertion and operation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -14,5 +14,35 @@ let score ="65qw"
 
 let isName= 8
 let stringName= String (isName)
-console.log(stringName)
-console.log(typeof stringName)
+// console.log(stringName)
+// console.log(typeof stringName)
+
+
+
+
+// ******************** Operation ********************
+// console.log(1+2)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+2+"3")
+// console.log("1"+2+3)
+// console.log(6%5)
+console.log(+true)
+console.log(typeof +"")
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+
+let x3 = 3;
+const y3 = ++x3;
+// x is 4; y is 4
+
+let x4 = 3n;
+const y4 = ++x4;
+// x2 is 4n; y2 is 4n
