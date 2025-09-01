d73f5cc2b1db0909c7e5979ca8ba6a380a8078ea
subhajit2509 <subhajitchess25@gmail.com>
2025-09-01 11:36:36 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name ="subhajit"
+let age =21
+let isLoggedIn= true
+
+// numder range 2 to power 53
+// bignt
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+
+
+// object
+
+// console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
