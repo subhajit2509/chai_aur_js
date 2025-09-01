5a48e1c15563d857e5a16be3690a5c020f771684
subhajit2509 <subhajitchess25@gmail.com>
2025-09-01 12:26:44 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score ="65qw"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  //it is showing valueInNumber is a number datatype but actully no number is being printed
+
+// let loggedIn=""
+// let boolLoggedIn= Boolean(loggedIn)
+// console.log(boolLoggedIn)
+
+let isName= 8
+let stringName= String (isName)
+console.log(stringName)
+console.log(typeof stringName)
